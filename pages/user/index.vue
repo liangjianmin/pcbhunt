0ca125e19bcb29d846d6b2cd07ec22df05f6ed06
liangjianmin <1129235084@qq.com>
2020-11-04 17:42:17 +0800

    css

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,6 +1,108 @@
 <template>
-	<view class="user-index">
-		
+	<view class="user-order">
+		<view class="user-search row bothSide verCenter">
+			<view class="search">
+				<input type="text" value="" placeholder="请输入订单编号/文件名" placeholder-style="color:#CCCCCC;" class="inp"/>
+				<text class="iconfont iconShape"></text>
+			</view>
+			<view class="btn">搜索</view>
+		</view>	
+		<view class="tab row avarage verCenter">
+			<view class="box curr row rowCenter verCenter">确认中(5)</view>
+			<view class="box row rowCenter verCenter">已下单(5)</view>
+		</view>
+		<view class="list">
+			<view class="box row">
+				<view class="time column rowCenter">
+					<text class="t1 row rowCenter verCenter">下单</text>
+					<text class="t2 row rowCenter verCenter">07-15</text>
+				</view>
+				<view class="txt column">
+					<text class="t1">订单包：39994949224</text>
+					<view>
+						<text class="t2">金额：300.00</text>
+						<text class="t3">子订单：3</text>
+					</view>
+					<view class="bar row verCenter">
+						<text class="t4">预计交货：2020-10-20 18:00</text>
+						<image src="../../static/24h.png" mode="aspectFill" class="im"></image>
+					</view>
+				</view>
+				<view class="status1 row rowCenter verCenter">已支付</view>
+			</view>
+			<view class="box row">
+				<view class="time column rowCenter">
+					<text class="t1 row rowCenter verCenter">下单</text>
+					<text class="t2 row rowCenter verCenter">07-15</text>
+				</view>
+				<view class="txt column">
+					<text class="t1">订单包：39994949224</text>
+					<view>
+						<text class="t2">金额：300.00</text>
+						<text class="t3">子订单：3</text>
+					</view>
+					<view class="bar row verCenter">
+						<text class="t4">预计交货：2020-10-20 18:00</text>
+						<image src="../../static/48h.png" mode="aspectFill" class="im"></image>
+					</view>
+				</view>
+				<view class="status2 row rowCenter verCenter">审单中</view>
+			</view>
+			<view class="box row">
+				<view class="time column rowCenter">
+					<text class="t1 row rowCenter verCenter">下单</text>
+					<text class="t2 row rowCenter verCenter">07-15</text>
+				</view>
+				<view class="txt column">
+					<text class="t1">订单包：39994949224</text>
+					<view>
+						<text class="t2">金额：300.00</text>
+						<text class="t3">子订单：3</text>
+					</view>
+					<view class="bar row verCenter">
+						<text class="t4">预计交货：2020-10-20 18:00</text>
+						<image src="../../static/48h.png" mode="aspectFill" class="im"></image>
+					</view>
+				</view>
+				<view class="status3 row rowCenter verCenter">余额支付</view>
+			</view>
+			<view class="box row">
+				<view class="time column rowCenter">
+					<text class="t1 row rowCenter verCenter">下单</text>
+					<text class="t2 row rowCenter verCenter">07-15</text>
+				</view>
+				<view class="txt column">
+					<text class="t1">订单包：39994949224</text>
+					<view>
+						<text class="t2">金额：300.00</text>
+						<text class="t3">子订单：3</text>
+					</view>
+					<view class="bar row verCenter">
+						<text class="t4">预计交货：2020-10-20 18:00</text>
+						<image src="../../static/48h.png" mode="aspectFill" class="im"></image>
+					</view>
+				</view>
+				<view class="status1 row rowCenter verCenter">审单完成</view>
+			</view>
+			<view class="box row">
+				<view class="time column rowCenter">
+					<text class="t1 row rowCenter verCenter">下单</text>
+					<text class="t2 row rowCenter verCenter">07-15</text>
+				</view>
+				<view class="txt column">
+					<text class="t1">订单包：39994949224</text>
+					<view>
+						<text class="t2">金额：300.00</text>
+						<text class="t3">子订单：3</text>
+					</view>
+					<view class="bar row verCenter">
+						<text class="t4">预计交货：2020-10-20 18:00</text>
+						<image src="../../static/48h.png" mode="aspectFill" class="im"></image>
+					</view>
+				</view>
+				<view class="status1 row rowCenter verCenter">审单完成</view>
+			</view>
+		</view>
 	</view>
 </template>
 
---
+++ pages/user/logistics.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="logistics">
+		<view class="top row rowCenter">
+			<view class="box row bothSide verCenter">
+				<view class="column">
+					<text class="t1">快递公司：顺丰快递</text>
+					<text class="t2">快递单号：5354158535132511</text>
+				</view>
+				<view class="pic">
+					<image src="../../static/sf.png" mode="aspectFill"></image>
+				</view>
+			</view>
+		</view>	
+		<view class="kefu row rowCenter verCenter">
+			<text class="iconfont iconiconoderkefu"></text>
+			<text class="t1">联系跟单客服</text>
+		</view>
+		<view class="list">
+			<view class="box column curr">
+				<text class="iconfont icontongyi"></text>
+				<text class="t1">订单已签收，期待再次为您服务</text>
+				<text class="t2">2020-10-09 14:25:20</text>
+			</view>
+			<view class="box column">
+				<text class="circle"></text>
+				<text class="t1">到达目的地网点广东深圳公司，快件将进行派送</text>
+				<text class="t2">2020-10-09 14:25:20</text>
+			</view>
+			<view class="box column">
+				<text class="circle"></text>
+				<text class="t1">在广东深圳公司进行发出扫描</text>
+				<text class="t2">2020-10-09 14:25:20</text>
+			</view>
+			<view class="box column">
+				<text class="circle"></text>
+				<text class="t1">在分拨中心广东深圳公司进行卸车扫描</text> 
+				<text class="t2">2020-10-09 14:25:20</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { API } from '@/util/api.js';
+export default {
+	data() {
+		return {};
+	},
+	onLoad(options) {},
+	methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../assets/css/user/logistics.scss';
+</style>
