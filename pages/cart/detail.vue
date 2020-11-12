807fad7433ccd55dfe3ae525c1d2acd6a0c9b1a4
liangjianmin <1129235084@qq.com>
2020-11-12 11:52:03 +0800

    css

---
+++ pages/cart/detail.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="cart-detail">
+		212
+	</view>
+</template>
+
+<script>
+import { API } from '@/util/api.js';
+import uniSwipeAction from '@/components/uni-swipe-action/uni-swipe-action.vue';
+import uniPopup from '@/components/uni-popup/uni-popup.vue';
+import uniPopupMessage from '@/components/uni-popup/uni-popup-message.vue';
+import uniPopupDialog from '@/components/uni-popup/uni-popup-dialog.vue';
+
+export default {
+	data() {
+		return {
+			
+		};
+	},
+	onLoad(options) {},
+	onShow() {},
+	methods: {
+		getData() {}
+	},
+	components: { uniSwipeAction, uniPopup, uniPopupMessage, uniPopupDialog }
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../assets/css/cart/detail.scss';
+</style>
