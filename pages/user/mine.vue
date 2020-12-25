266f0de8fb5ca5ecf72e75786146a23dfff8522d
liangjianmin <1129235084@qq.com>
2020-12-25 12:12:57 +0800

    mine

--- pages/user/mine.vue
+++ pages/user/mine.vue
@@ -8,17 +8,25 @@
 			</view>
 		</view>
 		<view class="list">
-			<view class="box row bothSide  verCenter">
-				<text class="t1">{{memberBase.SellerName}}</text>
-				<text class="iconfont iconyoujiantou"></text>
+			<view class="box row bothSide  verCenter" @click="tel(memberBase.SellerMobile)">
+				<text class="t1">{{ memberBase.SellerName }}</text>
+				<view class="row verCenter">
+					<text class="t2">{{ memberBase.SellerMobile }}</text>
+					<text class="iconfont iconyoujiantou"></text>
+				</view>
 			</view>
-			<view class="box row bothSide  verCenter">
-				<text class="t1">{{memberBase.FollowerName}}</text>
-				<text class="iconfont iconyoujiantou"></text>
+			<view class="box row bothSide  verCenter" @click="tel(memberBase.FollowerMobile)">
+				<text class="t1">{{ memberBase.FollowerName }}</text>
+				<view class="row verCenter">
+					<text class="t2">{{ memberBase.FollowerMobile }}</text>
+					<text class="iconfont iconyoujiantou"></text>
+				</view>
 			</view>
 			<view class="box row bothSide  verCenter">
 				<text class="t1">余额</text>
-				<text class="iconfont iconyoujiantou"></text>
+				<view class="row verCenter">
+					<text class="t2">¥ {{ memberBase.Balance || 0.0 }}</text>
+				</view>
 			</view>
 		</view>
 		<view class="btn row rowCenter verCenter" @click="signout()">退出登录</view>
@@ -45,6 +53,11 @@ export default {
 				if (res.Code === 200) {
 					this.memberBase = res.Data;
 				}
+			});
+		},
+		tel(val) {
+			uni.makePhoneCall({
+				phoneNumber: val
 			});
 		},
 		signout() {
